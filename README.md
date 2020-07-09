286afb73b6812a3248cc2773e8d7620b463628a1
wesleynguyen2511 <dragon251100@gmail.com>
2020-07-09 08:16:35 +0700

    1st upload

---
+++ README.md
@@ -0,0 +1,16 @@
+# Welcome to vietthuc Repository
+
+This Repository contains the sample project written in Java using
+for the demonstration of Continuous Integration Process (CI)
+It is a part of Software Engineering course in Semester 4 at FPT
+Unisersity, HCM Campus.
+
+## You will find in this repo:
+* MathUtil class
+* JUnit Test (TDD demonstration)
+* gitignore file
+
+## Contact me via
+[GitHub](https://github.com/wesleynguyen2511)
+
+#### © 2020 by vietthuc
---
+++ src/thuc/util/MathUtil.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package thuc.util;
+
+/**
+ *
+ * @author Judy
+ */
+//class nay chua cac ham tien ich ho tro cac phep tinh toan
+//vi may la tien ich ham y dung chung cho moi noi
+//do do chung may la static
+public class MathUtil {
+    //ham giai thua n1= 1,2,3...n bung no ket qua rat nhanh
+    //nen tran int (2 ti 1) som, ta tra long co 15! thoi
+    public static long computeFactorial(int n){
+        //cu viet, sai dung tu tu tinh sau
+        long result = 1;
+        for (int i = 0; i < 10; i++) {
+            result *=i;
+        }
+        return result;
+    }
+}
